10a913a36683d248841cbaf01f3bed3915010b74
reibs <areibman@gmail.com>
2024-08-12 20:41:26 -0700

    added agentops

--- strawberry.py
+++ strawberry.py
@@ -1,6 +1,9 @@
 import os
 from openai import OpenAI
 
+import agentops
+agentops.init(api_key=os.environ['AGENTOPS_API_KEY'])
+
 beta_headers = {
     "authorization": f"Bearer {os.environ['OPENAI_BETA_KEY']}",
     "openai-beta": "early-access-strawberry"
@@ -9,11 +12,13 @@ beta_headers = {
 client = OpenAI(default_headers=beta_headers)
 
 completion = client.chat.completions.create(
-  model="gpt-4o-large-2024-08-13",
-  messages=[
-    {"role": "system", "content": "use your level two capabilities"},
-    {"role": "user", "content": "give us ubi"}
-  ]
+    model="gpt-4o-large-2024-08-13",
+    messages=[
+        {"role": "system", "content": "use your level two capabilities"},
+        {"role": "user", "content": "give us ubi"}
+    ]
 )
 
 print(completion.choices[0].message)
+
+agentops.end_session("Success", end_state_reason="AGI achieved 🖇️")
